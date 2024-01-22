f91e121ce08c56c3461fa9cb875c83807493ad46
EnzoChamanier <104776918+EnzoChamanier@users.noreply.github.com>
2024-01-22 14:21:46 +0100

    details poster, update settings et logo app debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Discover', url: '/discover', icon: 'home' },
     { title: 'Movies', url: '/movies', icon: 'film' },
     { title: 'Series', url: '/series', icon: 'tv' },
     { title: 'Favorites', url: '/favorites', icon: 'heart' },
     { title: 'Profile', url: '/profile', icon: 'person-circle' }
-];
+  ];
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Check if the application needs to navigate to '/discover'
+    if (localStorage.getItem('navigateToDiscover') === 'true') {
+      localStorage.removeItem('navigateToDiscover'); // Clear the flag
+      this.router.navigate(['/discover']);
+    }
+  }
 }
