35a7aa651f85b0b1e8f55e42f919e93f42da973f
EnzoChamanier <104776918+EnzoChamanier@users.noreply.github.com>
2024-01-23 21:16:09 +0100

    profile in settings, theme

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -8,12 +8,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage {
+  // Propriété pour stocker le thème actuel
+  theme: string = 'light'; // Valeur par défaut pour le thème
 
   constructor(
     private loadingCtrl: LoadingController,
     private router: Router
-  ) {}
+  ) {
+    // Chargez le thème préféré de l'utilisateur au démarrage
+    this.loadPreferredTheme();
+  }
 
+  // Méthode pour charger le thème préféré de l'utilisateur
+  loadPreferredTheme() {
+    const savedTheme = localStorage.getItem('preferredTheme') || 'light'; // 'light' est la valeur par défaut
+    this.theme = savedTheme;
+    // Appliquez le thème ici, si nécessaire
+  }
+
+  // Méthode pour gérer le changement de thème
+  onThemeChange(selectedTheme: string) {
+    this.theme = selectedTheme;
+    this.savePreferredTheme(selectedTheme);
+    // Appliquez le thème ici, si nécessaire
+  }
+
+  // Méthode pour sauvegarder le thème préféré de l'utilisateur
+  savePreferredTheme(theme: string) {
+    localStorage.setItem('preferredTheme', theme);
+  }
+
+  // Méthode pour redémarrer l'application
   async loadingRestart() {
     const loading = await this.loadingCtrl.create({
       message: 'Redémarrage dans 3 secondes...',
@@ -29,4 +54,5 @@ export class SettingsPage {
       window.location.reload();
     });
   }
+  
 }
