0b51af8530e3f96453fe846ef4f3252753e795c5
EnzoChamanier <104776918+EnzoChamanier@users.noreply.github.com>
2024-01-25 10:29:39 +0100

    add camera, select in galery

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Appel de cette fonction pour définir les éléments PWA
+defineCustomElements(window);
+
 
 if (environment.production) {
   enableProdMode();
